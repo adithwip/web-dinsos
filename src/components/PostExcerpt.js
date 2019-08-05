93e7d73cc756016f66034fb31d88b168df8cb38f
adithwip <adithwidyapradipta@gmail.com>
2019-08-05 23:02:41 +0700

    improve post excerpt

--- src/components/PostExcerpt.js
+++ src/components/PostExcerpt.js
@@ -1,11 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+import styled from "styled-components"
 
 import Container from "../layouts/Container"
 import Item from "../layouts/Item"
 
+const ThumbnailBackgroundImage = styled(BackgroundImage)`
+  width: 100%;
+  height: 192px;
+  background-position: bottom center;
+  background-repeat: repeat-y;
+  background-size: cover;
+`
+
 const PostExcerpt = props => (
   <Container flexDirection="column">
+    <Item>
+      <ThumbnailBackgroundImage
+        fluid={props.data.frontmatter.thumbnail.childImageSharp.fluid}
+      />
+    </Item>
     <Item>
       <Link to={props.data.fields.slug}>
         <h4>{props.data.frontmatter.title}</h4>
