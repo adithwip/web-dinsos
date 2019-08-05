6efc1803437fe47b51fee14f35788f1d49994860
adithwip <adithwidyapradipta@gmail.com>
2019-08-05 20:20:39 +0700

    use gatsby-background-image and react-slick

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,69 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Slider from "react-slick"
+import BackgroundImage from 'gatsby-background-image'
+import styled from 'styled-components'
 
 import Layout from "../layouts/Layout"
-import Container from "../layouts/Container"
-import Item from "../layouts/Item"
-import PostExcerpt from "../components/PostExcerpt"
 
-export const query = graphql`
-  query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-      totalCount
-    }
-  }
+const StyledBackgroundImage = styled(BackgroundImage)`
+  width: 100%;
+  height: 60vh;
+  background-position: bottom center;
+  background-repeat: repeat-y;
+  background-size: cover;
 `
 
 const IndexPage = ({ data }) => {
-  const totalCount = data.allMarkdownRemark.totalCount
+  const images = [
+    { image: data.imageOne.childImageSharp.fluid },
+    { image: data.imageOne.childImageSharp.fluid },
+    { image: data.imageOne.childImageSharp.fluid },
+  ]
+
   return (
     <Layout
+      noPageContainer
       siteTitle="Blog Page"
       siteDescription="Homepage of Adith Widya Pradipta's blog"
     >
-      <Container flexDirection="column">
-        <Item>
-          <h4>
-            {totalCount} {totalCount > 0 ? "Posts" : "Post"}
-          </h4>
-        </Item>
-        <Item>
-          <Container flexDirection="column" spacing={8} column={2}>
-            {data.allMarkdownRemark.edges.map(({ node }) => (
-              <Item key={node.id}>
-                <PostExcerpt data={node} />
-              </Item>
-            ))}
-          </Container>
-        </Item>
-      </Container>
+      <Slider dots={true} infinite={true} speed={500} arrows={false}>
+        {images.map(({ image }) => {
+          return (
+            <StyledBackgroundImage
+              Tag="section"
+              fluid={image}
+              backgroundColor={`#040e18`}
+            />
+          )
+        })}
+      </Slider>
     </Layout>
   )
 }
 
 export default IndexPage
+
+export const squareImage = graphql`
+  fragment fluidImage on File {
+    childImageSharp {
+      fluid(maxWidth: 1200) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+`
+
+export const query = graphql`
+  query {
+    imageOne: file(relativePath: { eq: "images/image-one.jpg" }) {
+      ...fluidImage
+    }
+    imageTwo: file(relativePath: { eq: "images/image-two.jpg" }) {
+      ...fluidImage
+    }
+    imageThree: file(relativePath: { eq: "images/image-three.jpg" }) {
+      ...fluidImage
+    }
+  }
+`
