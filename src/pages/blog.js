66af353d95fe16f64cab684e70d8f2753e1306cb
adithwip <adithwidyapradipta@gmail.com>
2019-08-05 20:19:03 +0700

    move blog component to blog file in pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,68 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../layouts/Layout"
+import Container from "../layouts/Container"
+import Item from "../layouts/Item"
+import PostExcerpt from "../components/PostExcerpt"
 
-const BlogPage = () => (
-  <Layout
-    siteTitle="Blog Dummy Page"
-    siteDescription="A dummy page for testing"
-  >
-    <h1>This is Blog Pag</h1>
-    <h3>I will write a lot of things here</h3>
-  </Layout>
-)
+const BlogPage = ({ data }) => {
+  const totalCount = data.allMarkdownRemark.totalCount
+
+  return (
+    <Layout
+      siteTitle="Blog Page"
+      siteDescription="Homepage of Adith Widya Pradipta's blog"
+    >
+      <Container flexDirection="column">
+        <Item>
+          <h4>
+            {totalCount} {totalCount > 0 ? "Posts" : "Post"}
+          </h4>
+        </Item>
+        <Item>
+          <Container flexDirection="column" spacing={8} column={2}>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <Item key={node.id}>
+                <PostExcerpt data={node} />
+              </Item>
+            ))}
+          </Container>
+        </Item>
+      </Container>
+    </Layout>
+  )
+}
 
 export default BlogPage
+
+export const squareImage = graphql`
+  fragment fluidImage on File {
+    childImageSharp {
+      fluid(maxWidth: 1200) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+`
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+      totalCount
+    }
+  }
+`
