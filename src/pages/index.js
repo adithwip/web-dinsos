bf63cda56e8f859c0afee30d7266ad334fd8f3e6
adithwip <adithwidyapradipta@gmail.com>
2019-08-15 17:59:33 +0700

    revamp the homepage!!!!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,134 +1,86 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
-import Slider from "react-slick"
-import BackgroundImage from "gatsby-background-image"
-import styled from "styled-components"
-import PageContainer from "../layouts/PageContainer"
 
+import Grid from "@material-ui/core/Grid"
+
+import CardMenu from "../components/CardMenu"
+import ImageCarouselCard from "../components/ImageCarouselCard"
 import Layout from "../layouts/Layout"
-import Container from "../layouts/Container"
-import Item from "../layouts/Item"
-import PostExcerpt from "../components/PostExcerpt"
-
-const StyledBackgroundImage = styled(BackgroundImage)`
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 70vh;
-  background-position: bottom center;
-  background-repeat: repeat-y;
-  background-size: cover;
-
-  @media (max-width: 576px) {
-    height: 50vh;
-  }
-`
 
 const IndexPage = ({ data }) => {
+  const upperMenu = (
+    <Grid item style={{ width: "100%" }}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs={12} sm={6} md={4}>
+          <ImageCarouselCard />
+        </Grid>
+
+        <Grid item xs={6} sm={6} md={4}>
+          <Grid container spacing={2} direction="column">
+            <Grid item>
+              <CardMenu menu="kontak" to="/about" />
+            </Grid>
+            <Grid item>
+              <CardMenu
+                menu="lks"
+                href="https://rasetprojects.com/pusdatin/home_grafik.html"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={6} sm={6} md={4}>
+          <Grid container spacing={2} direction="column">
+            <Grid item>
+              <CardMenu menu="pusat-berita" to="/blog" />
+            </Grid>
+            <Grid item>
+              <CardMenu menu="struktur-organisasi" to="/blog" />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+
+  const lowerMenu = (
+    <Grid item style={{ width: "100%" }}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs={6} sm={4} md={3}>
+          <CardMenu menu="lpse" href="http://lpse.jakarta.go.id/eproc/" />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3}>
+          <CardMenu menu="jakarta" href="http://smartcity.jakarta.go.id/" />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3}>
+          <CardMenu menu="open-data" href="http://data.jakarta.go.id/" />
+        </Grid>
+        <Grid item xs={6} sm={4} md={3}>
+          <CardMenu menu="lpse" to="/about" />
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+
   return (
     <Layout
-      noPageContainer
+      noGrid
       siteTitle="Blog Page"
-      siteDescription="Homepage of Dinas Sosial DKI Jakarta"
+      siteDescription="Blog Page of Dinas Sosial DKI Jakarta"
     >
-      <Slider dots={true} infinite={true} speed={500} arrows={false}>
-        {data.allMarkdownRemark.edges.map(({ node }) => {
-          return (
-            <StyledBackgroundImage
-              Tag="section"
-              className="background-image"
-              fluid={node.frontmatter.thumbnail.childImageSharp.fluid}
-              backgroundColor={`#040e18`}
-            >
-              <Container
-                justify="center"
-                alignItems="flex-end"
-                fullHeight
-                spacing={32}
-              >
-                <Item>
-                  <Link
-                    style={{
-                      textShadow: "unset",
-                      color: "white",
-                      backgroundColor: "black",
-                      padding: "8px 16px",
-                      borderRadius: 20,
-                      marginBottom: 16,
-                    }}
-                    to={node.fields.slug}
-                  >
-                    Klik untuk ke Berita
-                  </Link>
-                </Item>
-              </Container>
-            </StyledBackgroundImage>
-          )
-        })}
-      </Slider>
-
-      <PageContainer>
-        <Container flexDirection="column">
-          <Item>
-            <h2>Berita dan Informasi</h2>
-          </Item>
-          <Item>
-            <Container flexDirection="column" spacing={16} column={2}>
-              {data.allMarkdownRemark.edges.map(({ node }) => (
-                <Item key={node.id}>
-                  <PostExcerpt data={node} />
-                </Item>
-              ))}
-            </Container>
-          </Item>
-        </Container>
-      </PageContainer>
+      <Grid
+        container
+        direction="column"
+        style={{ margin: "64px auto", width: "100%" }}
+        justify="center"
+        alignContent="center"
+        alignItems="center"
+        spacing={4}
+      >
+        {upperMenu}
+        {lowerMenu}
+      </Grid>
     </Layout>
   )
 }
 
 export default IndexPage
-
-export const squareImage = graphql`
-  fragment fluidImage on File {
-    childImageSharp {
-      fluid(maxWidth: 1200) {
-        ...GatsbyImageSharpFluid
-      }
-    }
-  }
-`
-
-export const query = graphql`
-  query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-            thumbnail {
-              ...fluidImage
-            }
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-      totalCount
-    }
-
-    imageOne: file(relativePath: { eq: "images/image-one.jpg" }) {
-      ...fluidImage
-    }
-    imageTwo: file(relativePath: { eq: "images/image-two.jpg" }) {
-      ...fluidImage
-    }
-    imageThree: file(relativePath: { eq: "images/image-three.jpg" }) {
-      ...fluidImage
-    }
-  }
-`
