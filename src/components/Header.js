f0cf29b11d18356f7b64ee79f7b12b9b29227246
adithwip <adithwidyapradipta@gmail.com>
2019-08-15 17:56:34 +0700

    modify and improve header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import styled from "styled-components"
+
+import Typography from "@material-ui/core/Typography"
 import Img from "gatsby-image"
 import MediaQuery from "react-responsive"
 
+import AppBar from "./AppBar"
 import Container from "../layouts/Container"
 import Item from "../layouts/Item"
 
 const StyledHeader = styled.header``
 
 const Header = () => {
-  const [open, navbarToggle] = useState(false)
-
   const data = useStaticQuery(
     graphql`
       query {
@@ -36,81 +37,63 @@ const Header = () => {
 
   return (
     <StyledHeader id="header">
-      <Container alignItems="center" justify="space-between">
-        <Link to="/">
-          <Item>
-            <Container alignItems="center" spacing={16}>
-              <Item>
-                <Img fixed={data.headerLogo.childImageSharp.fixed} />
-              </Item>
-              <Item>
-                <span
-                  style={{ fontWeight: "bold", fontSize: 20, color: "black" }}
-                >
-                  Dinas Sosial
-                </span>
-              </Item>
-            </Container>
-          </Item>
-        </Link>
+      <MediaQuery minDeviceWidth={320} maxDeviceWidth={767}>
+        <AppBar />
+      </MediaQuery>
 
-        <MediaQuery minDeviceWidth={320} maxDeviceWidth={767}>
+      <MediaQuery minDeviceWidth={768}>
+        <Container
+          style={{ padding: "16px 64px" }}
+          alignItems="center"
+          justify="space-between"
+        >
           <Item>
-            <button onClick={() => navbarToggle(!open)}>Menu</button>
-          </Item>
-          {open && (
-            <div
-              style={{
-                boxSizing: "border-box",
-                paddingLeft: 16,
-                paddingRight: 16,
-                width: "60%",
-                backgroundColor: "white",
-                // display: flex;
-                // flex-direction: column;
-                position: "fixed",
-                top: 0,
-                right: 0,
-                zIndex: 250,
-                height: "100vh",
-              }}
-            >
-              <Container flexDirection="column">
+            <Link to="/">
+              <Container alignItems="center" spacing={16}>
                 <Item>
-                  <button onClick={() => navbarToggle(!open)}>Close</button>
+                  <Img fixed={data.headerLogo.childImageSharp.fixed} />
                 </Item>
-                <Item>Menu Item</Item>
-                <Item>Menu Item</Item>
-                <Item>Menu Item</Item>
+                <Item>
+                  <span
+                    style={{ fontWeight: "bold", fontSize: 20, color: "black" }}
+                  >
+                    Pusdatin Dinas Sosial DKI Jakarta
+                  </span>
+                </Item>
               </Container>
-            </div>
-          )}
-        </MediaQuery>
-
-        <MediaQuery minDeviceWidth={768}>
+            </Link>
+          </Item>
           <Item>
             <Container spacing={16}>
               <Item>
-                <Link to="/">Home</Link>
+                <Link to="/">
+                  <Typography variant="button">Home</Typography>
+                </Link>
               </Item>
               <Item>
-                <Link to="/about">Kontak</Link>
+                <Link to="/about">
+                  <Typography variant="button">Kontak</Typography>
+                </Link>
               </Item>
               <Item>
                 <a href="https://rasetprojects.com/pusdatin/home_grafik.html">
-                  Pusat Data
+                  <Typography variant="button">Pusat Data</Typography>
                 </a>
               </Item>
               <Item>
-                <Link to="/blog">Berita</Link>
+                <Link to="/blog">
+                  <Typography variant="button">Berita</Typography>
+                </Link>
               </Item>
               <Item>
-                <Link to="/files">File System</Link>
+                <Link to="/files">
+                  <Typography variant="button">File System</Typography>
+                </Link>
               </Item>
             </Container>
           </Item>
-        </MediaQuery>
-      </Container>
+        </Container>
+      </MediaQuery>
     </StyledHeader>
   )
 }
