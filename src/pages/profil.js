dc6985357843adc8fc3b6d823399e140244919c8
Unknown <alimm.abdullah@gmail.com>
2019-10-09 20:05:11 +0700

    update profil page

--- src/pages/profil.js
+++ src/pages/profil.js
@@ -1,20 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 import styled from "styled-components"
+import Img from "gatsby-image"
 
 import Layout from "../layouts/Layout"
 import Paper from "@material-ui/core/Paper"
 
 const StyledPaper = styled(Paper)`
   padding: 32px 16px;
-`
+` 
+// const data = useStaticQuery(
+//   graphql`
+//     query {
+//       site {
+//         siteMetadata {
+//           title
+//         }
+//       }
+
+//       strukturOrganisasi: file(relativePath: { eq: "images/struktur-organisasi.png" }) {
+//         childImageSharp {
+//           fluid {
+//             ...GatsbyImageSharpFluid
+//           }
+//         }
+//       }
+//     }
+//   `
+// )
 
 const ProfilPage = () => (
   <Layout
     siteTitle="Profil"
     siteDescription="Will provide my readers about myself"
   >
-    <StyledPaper>
+    {/* <StyledPaper>
       <h1>Pusdatin Dinas Sosial Provinsi DKI Jakarta</h1>
       <p>Hubungi kami:</p>
       <ul>
@@ -55,7 +75,30 @@ const ProfilPage = () => (
           <Link to="/berita">Berita dan Informasi</Link>
         </li>
       </ul>
-    </StyledPaper>
+    </StyledPaper> */}
+    <h2>Pusat Data dan Informasi Jaminan Sosial, Dinas Sosial Provinsi DKI Jakarta</h2>
+    <h3>Visi</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <h3>Misi</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    
+    <h3>Struktur Organisasi</h3>
+    {/* <Img fixed={data.strukturOrganisasi.childImageSharp.Fluid} /> */}
+
   </Layout>
 )
 
