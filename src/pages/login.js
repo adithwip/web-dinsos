eb1ec37c99c16f16af0f8e194a30bd70c82818fb
Unknown <alimm.abdullah@gmail.com>
2019-10-10 00:21:20 +0700

    empty login page

---
+++ src/pages/login.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import styled from "styled-components"
+import Img from "gatsby-image"
+
+import Layout from "../layouts/Layout"
+import Paper from "@material-ui/core/Paper"
+
+const StyledPaper = styled(Paper)`
+  padding: 32px 16px;
+` 
+
+
+const LoginPage = () => {
+
+  return (
+    <Layout
+      siteTitle="Login - Pusdatin Jamsos"
+      siteDescription="Login Page"
+    >
+
+
+
+    </Layout>
+  )
+}
+
+export default LoginPage
