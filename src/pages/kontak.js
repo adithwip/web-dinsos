71712be26593610afc2ff3678112ddec554bec4c
Unknown <alimm.abdullah@gmail.com>
2019-11-05 07:22:13 +0700

    kontak page with api

--- src/pages/kontak.js
+++ src/pages/kontak.js
@@ -1,4 +1,5 @@
 import React from "react"
+import axios from "axios"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import styled from "styled-components"
 import Img from "gatsby-image"
@@ -18,6 +19,7 @@ import MailOutlineIcon from "@material-ui/icons/MailOutline"
 import TwitterIcon from "@material-ui/icons/Twitter"
 import FacebookIcon from "@material-ui/icons/Facebook"
 import PhoneIcon from "@material-ui/icons/Phone"
+import BusinessIcon from '@material-ui/icons/Business';
 
 const StyledPaper = styled(Paper)`
   padding: 32px 16px;
@@ -29,79 +31,109 @@ const NavButton = styled.a`
   margin: 0 4px;
   border: 1px solid gray;
 `
+class KontakPage extends React.Component {
+  
+  state = {
+    dataJson: null,
+    error: false,
+    loading: false,
+  }
 
-const KontakPage = () => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `
-  )
+  fetchData = () => {
+    this.setState({ loading: true })
+    axios
+      .get(`https://api.myjson.com/bins/6716o`, {
+        crossdomain: true,
+      })
+      .then(result => {
+        const { data } = result.data
+        this.setState({ dataJson : data, loading: false })
+      })
+      .catch(error => {
+        console.log(error)
+        this.setState({ loading: false, error: error })
+      })
+  }
 
-  return (
-    <Layout siteTitle="Kontak" siteDescription="Kontak & Hubungi Kami">
-      <h2>Kontak</h2>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={5}>
-          <div>
-            <List>
-              <ListItem>
-                <ListItemIcon>
-                  <MailOutlineIcon />
-                </ListItemIcon>
-                <ListItemText primary="pusdatinjamsosdki@gmail.com" />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <PhoneIcon />
-                </ListItemIcon>
-                <ListItemText primary="021-22684824" />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <TwitterIcon />
-                </ListItemIcon>
-                <a
-                  href="https://twitter.com/dinsosdki1?lang=en"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItemText primary="Twitter Dinsos DKI" />
-                </a>
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <FacebookIcon />
-                </ListItemIcon>
-                <a
-                  href="https://id-id.facebook.com/dinsosDKIJakarta"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ListItemText primary="Facebook Dinsos DKI" />
-                </a>
-              </ListItem>
-            </List>
-          </div>
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  render () {
+    const { dataJson, error, loading } = this.state
+
+    const address = !!dataJson && dataJson.address
+    const email = !!dataJson && dataJson.email
+    const phoneNumber = !!dataJson && dataJson.phone_number
+    const twitter = !!dataJson && dataJson.twitter
+    const facebook = !!dataJson && dataJson.facebook
+
+    return (
+      <Layout siteTitle="Kontak" siteDescription="Kontak & Hubungi Kami">
+        <h2>Kontak</h2>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={5}>
+            <div>
+              <List>
+                <ListItem>
+                  <ListItemIcon>
+                    <BusinessIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={address} />
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <MailOutlineIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={email} />
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <PhoneIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={phoneNumber} />
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <TwitterIcon />
+                  </ListItemIcon>
+                  <a
+                    href={twitter}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <ListItemText primary="Twitter Dinsos DKI" />
+                  </a>
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <FacebookIcon />
+                  </ListItemIcon>
+                  <a
+                    href={facebook}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <ListItemText primary="Facebook Dinsos DKI" />
+                  </a>
+                </ListItem>
+              </List>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} md={7}>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.819982213951!2d106.83455031413696!3d-6.154859362039576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f59245fac57d%3A0x43046ee5708f9c5!2sPUSDATIN%20JAMSOS!5e0!3m2!1sen!2sid!4v1570860514287!5m2!1sen!2sid"
+              width="100%"
+              height="380"
+              frameBorder="0"
+              allowfullscreen=""
+            ></iframe>
+          </Grid>
         </Grid>
-
-        <Grid item xs={12} md={7}>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.819982213951!2d106.83455031413696!3d-6.154859362039576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f59245fac57d%3A0x43046ee5708f9c5!2sPUSDATIN%20JAMSOS!5e0!3m2!1sen!2sid!4v1570860514287!5m2!1sen!2sid"
-            width="100%"
-            height="380"
-            frameBorder="0"
-            allowfullscreen=""
-          ></iframe>
-        </Grid>
-      </Grid>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
 }
 
 export default KontakPage
