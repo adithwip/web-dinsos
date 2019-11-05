9d1f0cc516faf890e7682bbd8468522238449711
Unknown <alimm.abdullah@gmail.com>
2019-11-05 13:04:38 +0700

    add chart fmotm by wilayah with api

---
+++ src/components/charts/fmotm/FMOTMByWilayahChart.js
@@ -0,0 +1,106 @@
+import React from "react"
+import axios from "axios"
+import Grid from "@material-ui/core/Grid"
+
+import ChartCard from "../../ChartCard"
+import { Chart } from "@bit/primefaces.primereact.chart"
+
+import Container from "../../../layouts/Container"
+import Item from "../../../layouts/Item"
+
+class FMOTMByWilayahChart extends React.Component {
+  state = {
+    dataJson: null,
+    error: false,
+    loading: false,
+  }
+
+  fetchDataAPI = () => {
+    this.setState({ loading: true })
+    const api = "http://ppds.pusdatin-dinsos.jakarta.go.id/api/summary/2019";
+    axios
+    //   .get(api, { crossdomain: true })
+      .get(`https://api.myjson.com/bins/t7fps`, { crossdomain: true })
+      .then(result => {
+        console.log(api, result)
+        const { data } = result.data
+        this.setState({
+          loading: false,
+          dataJson: data,
+        })
+      })
+      .catch(error => {
+        this.setState({ loading: false, error })
+      })
+  }
+
+  componentDidMount() {
+    this.fetchDataAPI()
+  }
+
+  render() {
+    const { dataJson, error, loading } = this.state
+
+    const extractData = (type, dataFromState) => {
+        let arr = []
+        console.log('dataFromState', dataFromState)
+        !!dataFromState && dataFromState.forEach(data => {
+            type === "kabupaten" && arr.push(data.kabupaten)
+            type === "total" && arr.push(data.total)
+        })
+
+        return arr
+    }
+
+    const randomColor = () => {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+    
+    const chartDataBar = {
+        labels: extractData('kabupaten', dataJson),
+        datasets: [
+          {
+            label: 'Fakir Miskin / Orang Tidak Mampu',
+            backgroundColor: [randomColor(), randomColor(),randomColor(),randomColor(),randomColor(),randomColor()],
+            data: extractData('total', dataJson)
+          }
+        ]
+      }
+  
+      const customOptions = {
+        legend : { display: false },
+        scales : {
+          xAxes:[{
+            ticks: { fontColor: "white" }
+          }],
+          yAxes:[{
+            ticks: { fontColor: "white" }
+          }]
+        }
+      }
+
+    return (
+      <ChartCard title="FMOTM Berdasarkan Wilayah">
+        <Grid
+          style={{ height: "100%" }}
+          container
+          direction="column"
+          justify="center"
+        >
+          <Container flexDirection="column" spacing={16}>
+            <Item flex={1}>
+              <Chart type="horizontalBar" data={chartDataBar} options={ customOptions } />
+            </Item>
+          </Container>
+        </Grid>
+      </ChartCard>
+    )
+  }
+}
+
+export default FMOTMByWilayahChart
