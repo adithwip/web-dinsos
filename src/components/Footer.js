385994050d945591842aae1baf40362bf5d0f54d
Unknown <alimm.abdullah@gmail.com>
2019-11-15 01:55:20 +0700

    footer - color properties

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,25 @@ import React from "react"
 import Container from "../layouts/Container"
 import Item from "../layouts/Item"
 
-const Footer = () => (
-  <Container justify="center">
+const Footer = (props) => (
+  <Container justify="center" 
+    style={{ 
+      backgroundColor: BackgroundColor(props.background),
+      color: Color(props.color) 
+    }}
+  >
     <Item>
-      <h6>&copy; Copyright - Pusdatin Dinas Sosial Provinsi DKI Jakarta</h6>
+      <h6 style={{ color: Color(props.color) }}>&copy; Copyright - Pusdatin Dinas Sosial Provinsi DKI Jakarta</h6>
     </Item>
   </Container>
 )
+
+const BackgroundColor = (color) => {
+  return color !== undefined ? color : "#fff";
+}
+
+const Color = (color) => {
+  return color !== undefined ? color : "#000";
+}
 
 export default Footer
