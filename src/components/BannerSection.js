9cd6f2abca0a3eecfe8b7185aea54d6ba7a4568d
Unknown <alimm.abdullah@gmail.com>
2019-11-15 17:22:48 +0700

    banner section, gallery section

---
+++ src/components/BannerSection.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const BannerSection = () => {
+  return (
+    <div>
+      <div style={{ width:'100%', height:'498px', backgroundColor:'#447694' }}>
+        <img src="https://wowslider.com/sliders/demo-77/data1/images/field175959_1920.jpg" width="100%" height="100%" />
+      </div>
+    </div>
+  )
+}
+
+export default BannerSection
