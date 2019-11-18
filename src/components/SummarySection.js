fb80fc1c6c5f05982a0abae72d2b31da5a69b0fe
Unknown <alimm.abdullah@gmail.com>
2019-11-19 04:44:16 +0700

    add social media section to summary section

--- src/components/SummarySection.js
+++ src/components/SummarySection.js
@@ -3,6 +3,8 @@ import axios from "axios"
 import styled from "styled-components"
 // import { Link } from "gatsby"
 import { Link } from "@reach/router"
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Grid from "@material-ui/core/Grid"
 import Card from "@material-ui/core/Card"
@@ -14,9 +16,14 @@ import PersonIcon from '@material-ui/icons/Person';
 import PaymentIcon from '@material-ui/icons/Payment';
 
 import Typography from "@material-ui/core/Typography"
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
 
 const StyleContainer = styled(Grid)`
-  background-color: #f6f6f6;
+  background-color: #fff;
   padding: 12px 32px;
   margin-bottom: 12px;
 `
@@ -35,23 +42,8 @@ const NewsCard = styled(Card)`
 class SummarySection extends React.Component {
   state = { dataJson: null, error: false, loading: false, page: 1 }
 
-//   fetchData = () => {
-//     this.setState({ loading: true })
-//     axios
-//       .get(`http://104.43.9.40:8089/api/v1/cms/news?perpage=8`, {
-//         crossdomain: true,
-//       })
-//       .then(result => {
-//         const { data } = result
-//         this.setState({ dataJson: data, loading: false })
-//       })
-//       .catch(error => {
-//         this.setState({ loading: false, error: error })
-//       })
-//   }
-
   componentDidMount() {
-    // this.fetchData()
+
   }
 
   render() {
@@ -64,50 +56,48 @@ class SummarySection extends React.Component {
           <h2>Data Terpadu Kesejahteraan Sosial</h2>
         </Grid>
 
-        <Grid container item spacing={3}>
-          <Grid container item xs={12} md={9} direction="column">
-            <Grid container item xs direction="row" justify="space-between" alignItem="stretch">
-                <Grid item xs style={{ padding:"8px" }}  >
-                    <Card style={{ padding:"24px", textAlign:"center", height:"120px" }} align="justify"> 
-                        <PeopleIcon/> <br/> <Typography>406.435 DTKS</Typography>
-                    </Card>
-                </Grid>
-                <Grid item xs style={{ padding:"8px" }}  >
-                    <Card style={{ padding:"24px", textAlign:"center", height:"120px" }} align="justify"> 
-                        <PersonIcon /> <br/> <Typography>1.571.352 ART</Typography>
-                    </Card>
-                </Grid>
-                <Grid item xs style={{ padding:"8px" }}  >
-                    <Card style={{ padding:"24px", textAlign:"center", height:"120px" }} align="justify"> 
-                        <HomeIcon/> <br/> <Typography>xxx.xxx</Typography>
-                    </Card>
-                </Grid>
+        <Grid container item spacing={3} align="flex-start">
+          <Grid item xs={12} md={8}>
+          <div style={{ display:"flex", alignItems:"center", justifyContent:"center", flexDirection:"row", flexWrap:"wrap", flexFlow:"row wrap", alignContent:"flex-end" }}>
+            <Grid item xs={12} md={4} style={{ padding:"8px" }}  >
+                <Card style={{ padding:"24px", textAlign:"center", height:"110px", backgroundColor:"#f8f8ff" }} align="justify"> 
+                    <PeopleIcon/> <br/> <Typography>406.435 DTKS</Typography>
+                </Card>
             </Grid>
-            <Grid container item xs direction="row" justify="space-between" alignItem="stretch">
-                <Grid item xs style={{ padding:"8px" }}  >
-                    <Card style={{ padding:"16px", textAlign:"center", height:"150px" }} align="justify"> 
-                        <PaymentIcon /> <br/> <Typography>x.xxx.xxxx <br/>Kartu Jakarta Pintar</Typography>
-                    </Card>
-                </Grid>
-                <Grid item xs style={{ padding:"8px" }}  >
-                    <Card style={{ padding:"16px", textAlign:"center", height:"150px" }} align="justify"> 
-                        <PaymentIcon /> <br/> <Typography>x.xxx.xxxx <br/>Kartu Lansia Jakarta</Typography>
-                    </Card>
-                </Grid>
-                <Grid item xs style={{ padding:"8px" }}  >
-                    <Card style={{ padding:"16px", textAlign:"center", height:"150px" }} align="justify"> 
-                        <PaymentIcon /> <br/> <Typography>x.xxx.xxxx <br/>Kartu Jakarta Mahasiswa Unggul</Typography>
-                    </Card>
-                </Grid>
-                <Grid item xs style={{ padding:"8px" }}  >
-                    <Card style={{ padding:"16px", textAlign:"center", height:"150px" }} align="justify"> 
-                        <PaymentIcon /> <br/> <Typography>x.xxx.xxxx <br/>Pemenuhan Kebutuhan Dasar Anak</Typography>
-                    </Card>
-                </Grid>
+            <Grid item xs={12} md={4} style={{ padding:"8px" }}  >
+                <Card style={{ padding:"24px", textAlign:"center", height:"110px", backgroundColor:"#f8f8ff" }} align="justify"> 
+                    <PersonIcon /> <br/> <Typography>1.571.352 ART</Typography>
+                </Card>
             </Grid>
+            <Grid item xs={12} md={4} style={{ padding:"8px" }}  >
+                <Card style={{ padding:"24px", textAlign:"center", height:"110px", backgroundColor:"#f8f8ff" }} align="justify"> 
+                    <HomeIcon/> <br/> <Typography>xxx.xxx</Typography>
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={3} style={{ padding:"8px" }}  >
+                <Card style={{ padding:"16px", textAlign:"center", height:"150px", backgroundColor:"#f8f8ff" }} align="justify"> 
+                    <PaymentIcon /> <br/> <Typography>x.xxx.xxxx <br/>Kartu Jakarta Pintar</Typography>
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={3} style={{ padding:"8px" }}  >
+                <Card style={{ padding:"16px", textAlign:"center", height:"150px", backgroundColor:"#f8f8ff" }} align="justify"> 
+                    <PaymentIcon /> <br/> <Typography>x.xxx.xxxx <br/>Kartu Lansia Jakarta</Typography>
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={3} style={{ padding:"8px" }}  >
+                <Card style={{ padding:"16px", textAlign:"center", height:"150px", backgroundColor:"#f8f8ff" }} align="justify"> 
+                    <PaymentIcon /> <br/> <Typography>x.xxx.xxxx <br/>Kartu Jakarta Mahasiswa Unggul</Typography>
+                </Card>
+            </Grid>
+            <Grid item xs={12} md={3} style={{ padding:"8px" }}  >
+                <Card style={{ padding:"16px", textAlign:"center", height:"150px", backgroundColor:"#f8f8ff" }} align="justify"> 
+                    <PaymentIcon /> <br/> <Typography>x.xxx.xxxx <br/>Pemenuhan Kebutuhan Dasar Anak</Typography>
+                </Card>
+            </Grid>
+          </div>
           </Grid>
-          <Grid item xs={12} md={3}>
-            
+          <Grid item xs={12} md={4}>
+            <SocialMediaSection />
           </Grid>
         </Grid>
 
@@ -115,5 +105,70 @@ class SummarySection extends React.Component {
     )
   }
 }
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        <Box p={3}>{children}</Box>
+      </Typography>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+  
+  const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.paper,
+    },
+  }));
+
+  const SocialMediaSection = () => {
+      const classes = useStyles();
+      const [value, setValue] = React.useState(0);
+    
+      const handleChange = (event, newValue) => {
+        setValue(newValue);
+      };
+      
+      return (
+        <Paper square>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Tabs value={value} onChange={handleChange} aria-label="socmed tabs" style={{ backgroundColor:"#0d47a1" }}>
+                    <Tab label="Instagram" {...a11yProps(0)} />
+                    <Tab label="Facebook" {...a11yProps(1)} />
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0} style={{ minHeight: "350px" }}>
+                Instagram
+            </TabPanel>
+            <TabPanel value={value} index={1} style={{ minHeight: "350px" }}>
+                Facebook
+            </TabPanel>
+        </div>
+        </Paper>
+      )
+  }
 
 export default SummarySection
