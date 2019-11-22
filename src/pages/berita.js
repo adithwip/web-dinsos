d65396b411f4256efcb00d341495885a093b013e
adithwip <adithwidyapradipta@gmail.com>
2019-11-22 07:15:48 +0700

    finalized berita page layout

--- src/pages/berita.js
+++ src/pages/berita.js
@@ -19,9 +19,8 @@ import PopularNews from "../components/PopularNews"
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-` 
+`
 const NewsCard = styled(Card)`
-  
   & div {
     width: 100%;
     padding: 16px;
@@ -62,70 +61,94 @@ class BeritaPage extends React.Component {
 
     const duplicateFirstNewsToArray = (firstNews, arrayLen) => {
       const arr = []
-      for (let i =0; i < arrayLen; i++) {
+      for (let i = 0; i < arrayLen; i++) {
         arr.push(firstNews)
       }
       return arr
     }
     const hotNewsArrDummy = dataJson && duplicateFirstNewsToArray(firstNews, 4)
-    const popularNewsArrDummy = dataJson && duplicateFirstNewsToArray(firstNews, 10)
+    const popularNewsArrDummy =
+      dataJson && duplicateFirstNewsToArray(firstNews, 10)
 
     return (
       <Wrapper>
-      <Layout
-        noPageContainer
-        siteTitle="Berita"
-        siteDescription="Berita Pusdatin Dinas Sosial Provinsi DKI Jakarta"
-      >
-        <PageContainer>
-          <Grid container direction="column" style={{ minHeight:"500px", marginBottom:"25px" }}>
-            <Grid item xs>
-              <h2>Berita dan Informasi</h2>
-            </Grid>
-            <Grid item>
-              <Grid container spacing={2}>
-                <Grid item md={8}>
-                  <HotNews newsArr={hotNewsArrDummy} />
-                </Grid>
-                <Grid item md={4}>
-                  <PopularNews newsArr={popularNewsArrDummy} />
+        <Layout
+          noPageContainer
+          siteTitle="Berita"
+          siteDescription="Berita Pusdatin Dinas Sosial Provinsi DKI Jakarta"
+        >
+          <PageContainer>
+            <Grid
+              container
+              direction="column"
+              style={{ minHeight: "500px", marginBottom: "25px" }}
+            >
+              <Grid item xs>
+                <h2>Berita dan Informasi</h2>
+              </Grid>
+              <Grid item>
+                <Grid container spacing={2}>
+                  <Grid item md={8}>
+                    <Grid container spacing={2} direction="column">
+                      <Grid item>
+                        <HotNews newsArr={hotNewsArrDummy} />
+                      </Grid>
+                      <Grid item>
+                        <Grid container direction="row" spacing={2}>
+                          {!!daftarBerita &&
+                            daftarBerita.map(berita => {
+                              return (
+                                <Grid
+                                  item
+                                  key={berita.id}
+                                  md={6}
+                                  style={{ marginBottom: "10px" }}
+                                >
+                                  <Link
+                                    to={`news/${berita.id}`}
+                                    style={{ textDecoration: "none" }}
+                                  >
+                                    <NewsCard style={{ height: "100%" }}>
+                                      <div>
+                                        {!!berita.image && (
+                                          <img
+                                            src={berita.image}
+                                            width="100%"
+                                            height="180px"
+                                            alt="berita-pusdatin"
+                                          />
+                                        )}
+
+                                        <h3>{berita.title}</h3>
+                                        <span>
+                                          {new Date(
+                                            berita.created_at
+                                          ).toLocaleDateString("id-ID")}
+                                        </span>
+                                        <p>
+                                          {berita.content
+                                            .replace(/(<([^>]+)>)/gi, "")
+                                            .substring(0, 150)}{" "}
+                                          ...
+                                        </p>
+                                      </div>
+                                    </NewsCard>
+                                  </Link>
+                                </Grid>
+                              )
+                            })}
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item md={4}>
+                    <PopularNews newsArr={popularNewsArrDummy} />
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item>
-              <Grid container direction="row" spacing={2}>
-                  {!!daftarBerita &&
-                    daftarBerita.map(berita => {
-                      return (
-                        <Grid item key={berita.id} xs={12} sm={6} md={4} lg={3} style={{ marginBottom:"10px" }}>
-                          <Link
-                          to={`news/${berita.id}`}
-                          style={{ textDecoration: "none" }}
-                          >
-                          <NewsCard style={{ height:"100%" }}>
-                            <div>
-                              { !!berita.image && (
-                                <img
-                                  src={ berita.image }
-                                  width="100%" height="180px" alt="berita-pusdatin" />
-                              ) }
-                              
-                              <h3>{berita.title}</h3>                            
-                              <span>{ new Date(berita.created_at).toLocaleDateString("id-ID") }</span>
-                              <p>
-                                { (berita.content.replace(/(<([^>]+)>)/ig,"")).substring(0, 150) } ...
-                              </p>
-                            </div>
-                          </NewsCard>
-                        </Link>
-                        </Grid>                      
-                      )
-                    })}
-              </Grid>
-            </Grid>
-          </Grid>
-        </PageContainer>
-      </Layout>
+          </PageContainer>
+        </Layout>
         <KontakSection id="kontak" />
         <Footer background="#0A369D" color="#9E9E9E" />
       </Wrapper>
