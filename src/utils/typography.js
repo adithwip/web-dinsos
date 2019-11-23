08ae34e5eae430626b83596813e73883d57ff02a
adithwip <adithwidyapradipta@gmail.com>
2019-11-23 12:53:29 +0700

    fix typography js body fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,6 +3,13 @@ import githubTheme from "typography-theme-github"
 
 const typography = new Typography(githubTheme)
 
+typography.options.bodyFontFamily = ['Cabin']
+typography.options.overrideThemeStyles = () => ({
+  'p': {
+    letterSpacing: 'normal'
+  }
+})
+
 export const { scale, rhythm, options } = typography
 
 export default typography
