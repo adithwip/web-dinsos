65bd3e22b9b11db7ffdfdb4c8888d74ae919d651
adithwip <adithwidyapradipta@gmail.com>
2019-11-23 13:57:44 +0700

    fix give new body font family typographyjs

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,11 +3,11 @@ import githubTheme from "typography-theme-github"
 
 const typography = new Typography(githubTheme)
 
-typography.options.bodyFontFamily = ['Cabin']
+typography.options.bodyFontFamily = ["Cabin"]
 typography.options.overrideThemeStyles = () => ({
-  'p': {
-    letterSpacing: 'normal'
-  }
+  p: {
+    letterSpacing: "normal",
+  },
 })
 
 export const { scale, rhythm, options } = typography
