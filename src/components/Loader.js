e64420ce36bfdab404bfa2146877776b0d0a6ca3
adithwip <adithwidyapradipta@gmail.com>
2019-11-30 16:38:30 +0700

    loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./style.css"
+import styled from "styled-components"
+
+import PusdatinLogo from "../images/icon.png"
+
+const StyledLogo = styled.img`
+  transform: translate(-50%, -50%);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`
+
+const Loader = () => {
+  return (
+    <div class="loadingio-eclipse">
+      <div class="ldio-rpinwye8j0b">
+        <StyledLogo src={PusdatinLogo} alt="pusdatin-logo" width="70%" />
+        <div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
