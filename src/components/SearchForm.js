129436b86c4960de5e2b1310c9e1d5c9a9e3ab8d
adithwip <adithwidyapradipta@gmail.com>
2019-11-30 14:38:09 +0700

    styling and fix search button func

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,25 @@
 import React from "react"
+import styled from "styled-components"
 import { navigate } from "@reach/router"
-import "./style.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+
+import Grid from "@material-ui/core/Grid"
+
+const StyledForm = styled.form`
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  padding: 16px;
+`
+const SearchInput = styled.input`
+  background-color: transparent;
+  border: none;
+  width: 100%;
+
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid black;
+  }
+`
 
 const SearchForm = () => {
   const [searchTerm, setSearchTerm] = React.useState("")
@@ -11,14 +30,32 @@ const SearchForm = () => {
 
   const handleSubmit = e => {
     e.preventDefault()
-    navigate(`news-search/${searchTerm}`)
+    if (searchTerm.length > 3) {
+      navigate(`/news-search/${searchTerm}`)
+    } else {
+      alert("Minimum kata kunci tiga huruf...")
+    }
   }
   console.log(searchTerm)
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={searchTerm} onChange={handleChange} />
-      <input type="submit" value="Cari Berita" />
-    </form>
+    <StyledForm onSubmit={handleSubmit}> 
+      <Grid container spacing={1}>
+        <Grid item style={{ flex: 1 }}>
+          <SearchInput type="text" value={searchTerm} onChange={handleChange} placeholder="Cari Berita..."/>
+        </Grid>
+        <Grid item>
+          {/* <input type="submit" value="Cari Berita" /> */}
+          <FontAwesomeIcon
+            icon={faSearch}
+            size="1x"
+            style={{
+              color: "#0D1321"
+            }}
+            onClick={handleSubmit}
+          />
+        </Grid>
+      </Grid>
+    </StyledForm>
   )
 }
 
