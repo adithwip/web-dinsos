40c043e985c075c3004ac6c6ff59ce50701d2370
adithwip <adithwidyapradipta@gmail.com>
2019-12-04 23:12:36 +0700

    make berita page responsive

--- src/pages/berita.js
+++ src/pages/berita.js
@@ -1,12 +1,10 @@
 import React from "react"
 import axios from "axios"
-// import { Link } from "gatsby"
 import { Link } from "@reach/router"
+import MediaQuery from "react-responsive"
 import PageContainer from "../layouts/PageContainer"
 
 import Layout from "../layouts/Layout"
-import Container from "../layouts/Container"
-import Item from "../layouts/Item"
 import styled from "styled-components"
 import Card from "@material-ui/core/Card"
 import Grid from "@material-ui/core/Grid"
@@ -76,85 +74,110 @@ class BeritaPage extends React.Component {
 
     return (
       <Wrapper>
-        <Layout
-          noPageContainer
-          siteTitle="Berita"
-          siteDescription="Berita Pusdatin Dinas Sosial Provinsi DKI Jakarta"
-        >
-          <PageContainer>
-            <Grid
-              container
-              direction="column"
-              style={{ minHeight: "500px", marginBottom: "25px" }}
-            >
-              <Grid item xs>
-                <h2>Berita dan Informasi</h2>
+        <MediaQuery minDeviceWidth={320} maxDeviceWidth={767}>
+          <Layout
+            siteTitle="Berita"
+            siteDescription="Berita Pusdatin Dinas Sosial Provinsi DKI Jakarta"
+            mobileView
+          >
+            <Grid container spacing={1} direction="column">
+              <Grid item>
+                <SearchForm />
               </Grid>
               <Grid item>
-                <Grid container spacing={2}>
-                  <Grid item md={8}>
-                    <Grid container spacing={2} direction="column">
-                      <Grid item>
-                        <HotNews />
-                      </Grid>
-                      <Grid item>
-                        <Grid container direction="row" spacing={2}>
-                          {!!daftarBerita &&
-                            daftarBerita.map(berita => {
-                              return (
-                                <Grid
-                                  item
-                                  key={berita.id}
-                                  md={6}
-                                  style={{ marginBottom: "10px" }}
-                                >
-                                  <Link
-                                    to={`news/${berita.id}`}
-                                    style={{ textDecoration: "none" }}
-                                  >
-                                    <NewsCard style={{ height: "100%" }}>
-                                      <div>
-                                        {!!berita.image && (
-                                          <img
-                                            src={berita.image}
-                                            width="100%"
-                                            height="180px"
-                                            alt="berita-pusdatin"
-                                          />
-                                        )}
+                <HotNews />
+              </Grid>
+              <Grid item>
+                <h2>Berita Populer</h2>
+              </Grid>
+              <Grid item>
+                <PopularNews />
+              </Grid>
+            </Grid>
+          </Layout>
+        </MediaQuery>
 
-                                        <h3>{berita.title}</h3>
-                                        <span>
-                                          {new Date(
-                                            berita.created_at
-                                          ).toLocaleDateString("id-ID")}
-                                        </span>
-                                        <p>
-                                          {berita.content
-                                            .replace(/(<([^>]+)>)/gi, "")
-                                            .substring(0, 150)}{" "}
-                                          ...
-                                        </p>
-                                      </div>
-                                    </NewsCard>
-                                  </Link>
-                                </Grid>
-                              )
-                            })}
+        <MediaQuery minDeviceWidth={768}>
+          <Layout
+            noPageContainer
+            siteTitle="Berita"
+            siteDescription="Berita Pusdatin Dinas Sosial Provinsi DKI Jakarta"
+          >
+            <PageContainer>
+              <Grid
+                container
+                direction="column"
+                style={{ minHeight: "500px", marginBottom: "25px" }}
+              >
+                <Grid item xs>
+                  <h2>Berita dan Informasi</h2>
+                </Grid>
+                <Grid item>
+                  <Grid container spacing={2}>
+                    <Grid item md={8}>
+                      <Grid container spacing={2} direction="column">
+                        <Grid item>
+                          <HotNews />
+                        </Grid>
+                        <Grid item>
+                          <Grid container direction="row" spacing={2}>
+                            {!!daftarBerita &&
+                              daftarBerita.map(berita => {
+                                return (
+                                  <Grid
+                                    item
+                                    key={berita.id}
+                                    md={6}
+                                    style={{ marginBottom: "10px" }}
+                                  >
+                                    <Link
+                                      to={`news/${berita.id}`}
+                                      style={{ textDecoration: "none" }}
+                                    >
+                                      <NewsCard style={{ height: "100%" }}>
+                                        <div>
+                                          {!!berita.image && (
+                                            <img
+                                              src={berita.image}
+                                              width="100%"
+                                              height="180px"
+                                              alt="berita-pusdatin"
+                                            />
+                                          )}
+
+                                          <h3>{berita.title}</h3>
+                                          <span>
+                                            {new Date(
+                                              berita.created_at
+                                            ).toLocaleDateString("id-ID")}
+                                          </span>
+                                          <p>
+                                            {berita.content
+                                              .replace(/(<([^>]+)>)/gi, "")
+                                              .substring(0, 150)}{" "}
+                                            ...
+                                          </p>
+                                        </div>
+                                      </NewsCard>
+                                    </Link>
+                                  </Grid>
+                                )
+                              })}
+                          </Grid>
                         </Grid>
                       </Grid>
                     </Grid>
-                  </Grid>
-                  <Grid item md={4}>
-                    <SearchForm />
-                    <h3>Berita Populer</h3>
-                    <PopularNews />
+                    <Grid item md={4}>
+                      <SearchForm />
+                      <h3>Berita Populer</h3>
+                      <PopularNews />
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-          </PageContainer>
-        </Layout>
+            </PageContainer>
+          </Layout>
+        </MediaQuery>
         <KontakSection id="kontak" />
         <Footer background="#0A369D" color="#9E9E9E" />
       </Wrapper>
