ca8a607d090184c79ea20c521867665b055e8338
adithwip <adithwidyapradipta@gmail.com>
2019-12-04 22:15:55 +0700

    fix news detail layout on mobileView

--- src/components/NewsDetail.js
+++ src/components/NewsDetail.js
@@ -1,6 +1,7 @@
 import React from "react"
 import axios from "axios"
 import styled from "styled-components"
+import MediaQuery from "react-responsive"
 
 import Grid from "@material-ui/core/Grid"
 
@@ -72,57 +73,101 @@ class NewsDetail extends React.Component {
 
     return (
       <Wrapper>
-        <Layout
-          // siteTitle={post.frontmatter.title}
-          siteType="article"
-          // mobileFirst
-          // siteUrl={url}
-          // siteImage={image}
-          siteDescription={data && news.title}
-        >
-          {data != null ? (
-            <Grid container spacing={4} style={{ margin: "16px 0" }}>
-              <Grid item md={8}>
-                <Surface>
-                  <Grid container direction="column">
-                    <Grid item>
-                      <h1>{news.title}</h1>
+        <MediaQuery minDeviceWidth={320} maxDeviceWidth={767}>
+          <Layout
+            siteType="article"
+            siteDescription={data && news.title}
+            mobileView
+          >
+            {data != null ? (
+              <Grid container spacing={1} direction="column">
+                <Grid item>
+                  <h1>{news.title}</h1>
+                </Grid>
+                <Grid item>
+                  <StyledBgImg imgSrc={news.image} />
+                </Grid>
+                <Grid item>
+                  <Grid container>
+                    <Grid item style={{ flex: 1 }}>
+                      <p style={{ color: "#1CA086" }}>
+                        {news.created_at}
+                      </p>
                     </Grid>
-                    <Grid item>
-                      {/* <img src={news.image} alt="pusdatin" /> */}
-                      <StyledBgImg imgSrc={news.image} />
+                    <Grid item style={{ flex: 1 }}>
+                      <SocialMediaSharingButtons />
                     </Grid>
-                    <Grid item style={{ marginTop: "32px" }}>
-                      <Grid container>
-                        <Grid item style={{ flex: 1 }}>
-                          <p style={{ color: "#1CA086" }}>{news.created_at}</p>
-                        </Grid>
-                        <Grid item style={{ flex: 1 }}>
-                          <SocialMediaSharingButtons />
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <div
+                    dangerouslySetInnerHTML={{ __html: news.content }}
+                  />
+                </Grid>
+              </Grid>
+            ) : (
+              <div>Sedang Memuat Berita...</div>
+            )}
+          </Layout>
+        </MediaQuery>
+
+        <MediaQuery minDeviceWidth={768}>
+          <Layout
+            // siteTitle={post.frontmatter.title}
+            siteType="article"
+            // mobileFirst
+            // siteUrl={url}
+            // siteImage={image}
+            siteDescription={data && news.title}
+          >
+            {data != null ? (
+              <Grid container spacing={4} style={{ margin: "16px 0" }}>
+                <Grid item md={8}>
+                  <Surface>
+                    <Grid container direction="column">
+                      <Grid item>
+                        <h1>{news.title}</h1>
+                      </Grid>
+                      <Grid item>
+                        {/* <img src={news.image} alt="pusdatin" /> */}
+                        <StyledBgImg imgSrc={news.image} />
+                      </Grid>
+                      <Grid item style={{ marginTop: "32px" }}>
+                        <Grid container>
+                          <Grid item style={{ flex: 1 }}>
+                            <p style={{ color: "#1CA086" }}>
+                              {news.created_at}
+                            </p>
+                          </Grid>
+                          <Grid item style={{ flex: 1 }}>
+                            <SocialMediaSharingButtons />
+                          </Grid>
                         </Grid>
                       </Grid>
+                      <Grid item>
+                        <div
+                          dangerouslySetInnerHTML={{ __html: news.content }}
+                        />
+                      </Grid>
+                    </Grid>
+                  </Surface>
+                </Grid>
+                <Grid item md={4}>
+                  <Grid container direction="column" spacing={2}>
+                    <Grid item>
+                      <h2>Berita Populer</h2>
                     </Grid>
                     <Grid item>
-                      <div dangerouslySetInnerHTML={{ __html: news.content }} />
+                      <PopularNews maxNews={5} />
                     </Grid>
-                  </Grid>
-                </Surface>
-              </Grid>
-              <Grid item md={4}>
-                <Grid container direction="column" spacing={2}>
-                  <Grid item>
-                    <h2>Berita Populer</h2>
-                  </Grid>
-                  <Grid item>
-                    <PopularNews maxNews={5} />
                   </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-          ) : (
-            <div>Sedang Memuat Berita...</div>
-          )}
-        </Layout>
+            ) : (
+              <div>Sedang Memuat Berita...</div>
+            )}
+          </Layout>
+        </MediaQuery>
         <KontakSection id="kontak" />
         <Footer background="#0A369D" color="#9E9E9E" />
       </Wrapper>
